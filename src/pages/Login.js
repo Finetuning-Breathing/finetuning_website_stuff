1a92e1ed8db96612e31f9c8fcbbdd9e50be7b209
Yonathan Torres <70590429+YonYon2@users.noreply.github.com>
2022-12-06 22:54:02 -0500

    Login backbone finished. Need to add password reset function.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,11 @@
 import {useState} from "react";
 import Parse from "parse/dist/parse.min.js";
-import {Link, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 export default function Login(){
-    const { loggedIn } = useParams();
+    const { fail } = useParams();
+
+
     //state variables
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
@@ -22,11 +24,7 @@ export default function Login(){
         try {
             const loggedInUser = await Parse.User.logIn(username, password);
             // logIn returns the corresponding ParseUser object
-            alert(
-                `Success! User ${loggedInUser.get(
-                    'username'
-                )} has successfully signed in!`
-            );
+            //alert(`Success! User ${loggedInUser.get('username')} has successfully signed in!`);
             // To verify that this is in fact the current user, `current` can be used
             const currentUser = await Parse.User.current();
             console.log(loggedInUser === currentUser);
@@ -43,15 +41,31 @@ export default function Login(){
         }
     };
 
+    const doUserLogOut = async function () {
+        try {
+            await Parse.User.logOut();
+            // To verify that current user is now empty, currentAsync can be used
+            const currentUser = await Parse.User.current();
+            if (currentUser === null) {
+                alert('Success! No user is logged in anymore!');
+            }
+            // Update state variable holding current user
+            await getCurrentUser();
+            return true;
+        } catch (error) {
+            alert(`Error! ${error.message}`);
+            return false;
+        }
+    };
+
     // Functions used by the screen components
     const doUserRegistration = async function () {
         // Note that these values come from state variables that we've declared before
         try {
             // Since the signUp method returns a Promise, we need to call it using await
             const createdUser = await Parse.User.signUp(username, password);
-            alert(
-                `Success! User ${createdUser.getUsername()} was successfully created!`
-            );
+            setCurrentUser(createdUser);
+            //alert(`Success! User ${createdUser.getUsername()} was successfully created!`);
             return true;
         } catch (error) {
             // signUp can fail if any parameter is blank or failed a uniqueness check on the server
@@ -59,14 +73,9 @@ export default function Login(){
             return false;
         }
     };
-    if (loggedIn){
-        return <>
-            <h2 className="heading">Account</h2>
-            <div>
-
-            </div>
-        </>
-    } else return <>
+    //return code that displays the user login info when logged in
+    return <>
+        {currentUser === null && (
         <div className="container">
             <h2 className="heading">Account Login</h2>
             <div className="form_fields">
@@ -114,6 +123,23 @@ export default function Login(){
                     </button>
                 </div>
             </div>
-        </div>
+        </div>)}
+        {currentUser !== null && (
+            <div>
+                <h2 className="heading">User Screen</h2>
+                <divider/>
+                <h2 className="heading">{currentUser.get('username')}</h2>
+                <div className="form_buttons">
+                    <button
+                        onClick={() => doUserLogOut()}
+                        type="primary"
+                        className="form_button"
+                        color={'#208AEC'}
+                    >
+                        Log Out
+                    </button>
+                </div>
+            </div>
+        )}
     </>;
 }
