5e766fc9660942990e99a12d2818ef3c42d41aaa
Yonathan Torres <70590429+YonYon2@users.noreply.github.com>
2022-12-06 19:42:39 -0500

    Added react routers and created the skeleton of the website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-// Import Parse minified version
+import React from "react";
+import {Routes, Route, Link} from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Shop from "./pages/Shop";
+
+// Import Parse minified versio
 import Parse from 'parse/dist/parse.min.js';
 
 // Your Parse initialization configuration goes here
@@ -10,26 +15,29 @@ const PARSE_JAVASCRIPT_KEY = '7pCPR3fcdQpvYeOdNYi1QktQEyr51eiXel59TOwY';
 Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+          </ul>
+        </nav>
+        {/* A <Switch> looks through its children <Route>s and
+           renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/shop" element={<Shop/>} />
+        </Routes>
+      </div>
   );
 }
-
-export default App;
